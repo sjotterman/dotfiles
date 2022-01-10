1a671031bf6a572ad2f15700638c667dbbf3bec2
Samuel Otterman <samuel.otterman@brandingbrand.com>
2022-01-10 17:57:29 -0500

    hack for eslint LSP issue

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -22,3 +22,17 @@ require "user.whichkey"
 require "user.vim-qf"
 require "user.autocommands"
 
+
+-- temporary hack; neovim requires a column for diagnostics,
+-- but the current version of eslint lsp doesn't send it
+-- https://github.com/neovim/neovim/issues/16673
+-- If this needs to stay here for a while, it might be moved to one of the
+-- included LSP files
+vim.diagnostic.set = (function(orig)
+    return function(namespace, bufnr, diagnostics, opts)
+        for _, v in ipairs(diagnostics) do
+            v.col = v.col or 0
+        end
+        return orig(namespace, bufnr, diagnostics, opts)
+    end
+end)(vim.diagnostic.set)
