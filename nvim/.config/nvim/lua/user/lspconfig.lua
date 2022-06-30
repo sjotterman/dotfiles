dcbf4666d444c05834251863a86fd8529a883e1f
Samuel Otterman <samuel.otterman@brandingbrand.com>
2022-06-30 10:17:32 -0400

    Use prettier to format ts files

--- nvim/.config/nvim/lua/user/lspconfig.lua
+++ nvim/.config/nvim/lua/user/lspconfig.lua
@@ -48,6 +48,13 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  if client.name == "tsserver" then
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+      vim.cmd("nnoremap <silent><buffer> <Leader>lf :Prettier<CR>")
+      -- format on save
+      vim.cmd("autocmd BufWritePost <buffer> :Prettier<CR>")
+  end
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
@@ -72,31 +79,31 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 -- npm i -g pyright
-local servers = {  'pyright'  }
+local servers = {  'pyright', 'tsserver'}
 
-require("lspconfig").tsserver.setup({
-    on_attach = function(client, bufnr)
-      -- same as above, but disable formatting
-        client.resolved_capabilities.document_formatting = false
-        client.resolved_capabilities.document_range_formatting = false
-        -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-        local bufopts = { noremap=true, silent=true, buffer=bufnr }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-        vim.keymap.set('n', '<space>wl', function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, bufopts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    end,
-})
+-- require("lspconfig").tsserver.setup({
+--     on_attach = function(client, bufnr)
+--       -- same as above, but disable formatting
+--         client.resolved_capabilities.document_formatting = false
+--         client.resolved_capabilities.document_range_formatting = false
+--         -- Enable completion triggered by <c-x><c-o>
+--         vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+--
+--         local bufopts = { noremap=true, silent=true, buffer=bufnr }
+--         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+--         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+--         vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+--         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+--         vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+--         vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+--         vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+--         vim.keymap.set('n', '<space>wl', function()
+--           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--         end, bufopts)
+--         vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+--         vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+--     end,
+-- })
 require'lspconfig'.eslint.setup{
     on_attach = on_attach,
     settings = {
