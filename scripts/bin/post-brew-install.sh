a15cc326935939edb4948eecaef0a3a53fc219c5
Samuel Otterman <samuel.otterman@brandingbrand.com>
2022-07-06 14:22:28 -0400

    add global npm packages to postinstall script

--- scripts/bin/post-brew-install.sh
+++ scripts/bin/post-brew-install.sh
@@ -4,3 +4,9 @@
 gh extension install dlvhdr/gh-prs
 gh extension install mislav/gh-branch
 gh extension install seachicken/gh-poi
+
+# npm packages
+npm i -g pyright
+npm i -g tldr
+npm i -g vim-language-server
+npm i -g vscode-langservers-extracted
