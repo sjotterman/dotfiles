04431db5f7ffeaaf1c77c36dd53cbc9c6210504e
Samuel Otterman <samuel.otterman@brandingbrand.com>
2022-07-08 10:54:26 -0400

    add script to install cht.sh

--- scripts/bin/post-brew-install.sh
+++ scripts/bin/post-brew-install.sh
@@ -10,3 +10,15 @@ npm i -g pyright
 npm i -g tldr
 npm i -g vim-language-server
 npm i -g vscode-langservers-extracted
+
+
+# cht.sh https://github.com/chubin/cheat.sh
+PATH_DIR="$HOME/bin"  # or another directory on your $PATH
+if test -f "$PATH_DIR/cht.sh"; then
+    echo "cht.sh already installed"
+  else
+    echo "Installing cht.sh"
+    mkdir -p "$PATH_DIR"
+    curl https://cht.sh/:cht.sh > "$PATH_DIR/cht.sh"
+    chmod +x "$PATH_DIR/cht.sh"
+  fi
