5c54292807bcad659b6a1d845a5f182d6deca488
Samuel Otterman <sjotterman@gmail.com>
2022-07-31 13:09:44 -0400

    update lsp config for TS and Python

--- nvim/.config/nvim/lua/user/lspconfig.lua
+++ nvim/.config/nvim/lua/user/lspconfig.lua
@@ -79,32 +79,47 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 -- npm i -g pyright
-local servers = {  'pyright', 'tsserver'}
-local servers = {  'tsserver'}
+local python_root_files = {
+  'requirements.txt',
+  'pyproject.toml',
+  'setup.py',
+  'setup.cfg',
+  'Pipfile',
+  'pyrightconfig.json',
+}
 
--- require("lspconfig").tsserver.setup({
---     on_attach = function(client, bufnr)
---       -- same as above, but disable formatting
---         client.resolved_capabilities.document_formatting = false
---         client.resolved_capabilities.document_range_formatting = false
---         -- Enable completion triggered by <c-x><c-o>
---         vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
---
---         local bufopts = { noremap=true, silent=true, buffer=bufnr }
---         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
---         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
---         vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
---         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
---         vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
---         vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
---         vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
---         vim.keymap.set('n', '<space>wl', function()
---           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---         end, bufopts)
---         vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
---         vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
---     end,
--- })
+local configs = require('lspconfig/configs')
+local util = require('lspconfig/util')
+
+local path = util.path
+
+local function get_python_path(workspace)
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+  end
+
+  -- Find and use virtualenv in workspace directory.
+  for _, pattern in ipairs({'*', '.*'}) do
+    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
+    if match ~= '' then
+      return path.join(path.dirname(match), 'bin', 'python')
+    end
+  end
+
+  -- Fallback to system Python.
+  return exepath('python3') or exepath('python') or 'python'
+end
+
+
+require'lspconfig'.pyright.setup {
+    on_attach = on_attach,
+   root_dir = require'lspconfig'.util.root_pattern((python_root_files))
+}
+
+
+local servers = {  'tsserver', 'lspconfig'}
+
 require'lspconfig'.eslint.setup{
     on_attach = on_attach,
     settings = {
