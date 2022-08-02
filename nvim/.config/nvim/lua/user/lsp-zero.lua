8a8ee5220417d6d9e9a4a5847aeaf1dc4da7c69d
Samuel Otterman <sjotterman@gmail.com>
2022-08-02 13:34:25 -0400

    Set up null-ls for prettier formatting

--- nvim/.config/nvim/lua/user/lsp-zero.lua
+++ nvim/.config/nvim/lua/user/lsp-zero.lua
@@ -2,3 +2,22 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 lsp.setup()
+
+require 'lspconfig'.eslint.setup {
+  settings = {
+    workingDirectory = { mode = 'location' },
+    format = false,
+  },
+  root_dir = require 'lspconfig'.util.root_pattern(
+    '.git',
+    '.eslintrc.json',
+    '.eslintrc.js'
+  ),
+}
+
+require 'lspconfig'.tsserver.setup {
+  on_attach = function(client)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+  end,
+}
--- nvim/.config/nvim/lua/user/nvim-code-action-menu.lua
+++ nvim/.config/nvim/lua/user/nvim-code-action-menu.lua
@@ -1,2 +1,2 @@
-vim.g.code_action_menu_show_details = false
+vim.g.code_action_menu_show_details = true
 vim.g.code_action_menu_show_diff = true
