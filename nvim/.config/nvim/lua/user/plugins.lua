ba1c9ff4a53497a37ff13f21dba0ff21471bc2f7
Samuel Otterman <sjotterman@gmail.com>
2022-08-15 10:16:13 -0400

    install fidget neovim plugin

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -153,6 +153,9 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use { 'j-hui/fidget.nvim',  config = function()
+    require('fidget').setup()
+  end }
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
