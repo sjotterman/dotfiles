848c8340f0e7653a3586d9b59f716ff8e4d70fe0
Samuel Otterman <sjotterman@gmail.com>
2022-08-30 10:45:27 -0400

    add gh b extension to post install

--- scripts/bin/post-brew-install.sh
+++ scripts/bin/post-brew-install.sh
@@ -4,6 +4,7 @@
 gh extension install dlvhdr/gh-prs
 gh extension install mislav/gh-branch
 gh extension install seachicken/gh-poi
+gh extension install joaom00/gh-b
 
 # npm packages
 npm i -g pyright
