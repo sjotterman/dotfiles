d2c422a8e955d80ca45009fa197331daf8b6aa3a
Samuel Otterman <sjotterman@gmail.com>
2022-09-08 21:36:13 -0400

    neovim set guicursor=

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -61,3 +61,8 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+-- Some terminals are broken without this
+vim.cmd [[
+set guicursor=
+]]
