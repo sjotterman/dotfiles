2e98d2fa14dbe9286360a1593aba4256d6575342
Samuel Otterman <sjotterman@gmail.com>
2023-03-05 17:02:23 -0500

    gh-dash config

--- scripts/bin/install-gh-extensions.sh
+++ scripts/bin/install-gh-extensions.sh
@@ -4,3 +4,4 @@ gh extension install dlvhdr/gh-prs
 gh extension install mislav/gh-branch
 gh extension install seachicken/gh-poi
 gh extension install joaom00/gh-b
+gh extension install dlvhdr/gh-dash
