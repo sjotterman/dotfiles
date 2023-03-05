ccc4029ad079617fd31cff147e95cf18a7e9e9d2
Samuel Otterman <sjotterman@gmail.com>
2023-03-05 17:13:16 -0500

    Use default copilot accept keybind

--- nvim/.config/nvim/lua/user/copilot.lua
+++ nvim/.config/nvim/lua/user/copilot.lua
@@ -1,6 +1,6 @@
 vim.g.copilot_no_tab_map = true
 
-vim.api.nvim_set_keymap("i", "<M-Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+-- vim.api.nvim_set_keymap("i", "<M-Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
 
