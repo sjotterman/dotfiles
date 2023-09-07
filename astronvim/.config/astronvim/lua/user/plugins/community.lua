5eb02dae20f183afe50867a055d3e190de1d6aee
Samuel Otterman <sjotterman@gmail.com>
2023-09-07 17:54:24 -0400

    add plugins to astro

--- astronvim/.config/astronvim/lua/user/plugins/community.lua
+++ astronvim/.config/astronvim/lua/user/plugins/community.lua
@@ -13,6 +13,8 @@ return {
   { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.motion.mini-bracketed" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.harpoon" },
   { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
   { import = "astrocommunity.editing-support.vim-move" },
