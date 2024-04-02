de914e93139d3e02d3a29b80c961889898922c0b
Samuel Otterman <sjotterman@gmail.com>
2024-04-02 16:09:10 -0400

    disable neo-tree in fugitive

--- nvim/.config/astronvim_v4/lua/plugins/user.lua
+++ nvim/.config/astronvim_v4/lua/plugins/user.lua
@@ -14,4 +14,12 @@ return {
   -- You can disable default plugins as follows:
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+
+    {
+"nvim-neo-tree/neo-tree.nvim",
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.neo-tree"(plugin, opts) -- include the default astronvim config that calls the setup call
+      opts.open_files_do_not_replace_types = {"terminal", "trouble", "qf", "fugitive"}
+    end,
+  },
 }
