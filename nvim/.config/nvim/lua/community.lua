23e7f8dac9a2bc7637b7ef68e617e8225aa88aed
Samuel Otterman <sjotterman@gmail.com>
2024-07-02 11:55:37 -0400

    set up mise and use it for neovim

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -11,7 +11,6 @@ return {
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.git.diffview-nvim" },
-  { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.motion.mini-bracketed" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.harpoon" },
