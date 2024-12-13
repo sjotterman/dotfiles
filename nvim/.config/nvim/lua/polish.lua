c60ac6497150e881f1e5cbf40c8a0b6d4f037ac5
Samuel Otterman <sjotterman@gmail.com>
2024-12-13 17:53:36 -0500

    javascript lint command

--- nvim/.config/nvim/lua/polish.lua
+++ nvim/.config/nvim/lua/polish.lua
@@ -6,3 +6,15 @@
 vim.cmd [[command! Qa :qa]]
 vim.cmd [[command! Qall :qa]]
 vim.cmd [[command! Q :q]]
+
+vim.cmd [[
+augroup jslint
+  autocmd!
+  au BufNewFile,BufRead *.js set makeprg=yarn\ lint\ --format\ unix
+  au BufNewFile,BufRead *.jsx set makeprg=yarn\ lint\ --format\ unix
+  au BufNewFile,BufRead *.ts set makeprg=yarn\ lint\ --format\ unix
+  au BufNewFile,BufRead *.tsx set makeprg=yarn\ lint\ --format\ unix
+augroup END
+
+command! -nargs=0 Lint :make <bar> copen
+]]
