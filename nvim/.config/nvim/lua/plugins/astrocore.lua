b7b4b876f0b925f594df416b94237bc5b8339353
Samuel Otterman <sjotterman@gmail.com>
2025-06-19 15:20:44 -0400

    custom nvim polish commands

--- nvim/.config/nvim/lua/plugins/astrocore.lua
+++ nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -29,7 +29,7 @@ return {
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        wrap = true, -- sets vim.opt.wrap
         wildmode = "full:longest",
         foldcolumn = "0",
         scrolloff = 8,
