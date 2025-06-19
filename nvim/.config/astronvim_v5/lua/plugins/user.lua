e0dfb629fde0939d3571347988fbfa021288a1a0
Samuel Otterman <sjotterman@gmail.com>
2025-06-19 13:44:12 -0400

    astronvim_v5 config

--- nvim/.config/astronvim_v5/lua/plugins/user.lua
+++ nvim/.config/astronvim_v5/lua/plugins/user.lua
@@ -1,20 +1,9 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
 -- Here are some examples:
 
 ---@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
-
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
 
   -- == Examples of Overriding Plugins ==
 
@@ -44,4 +33,102 @@ return {
 
   -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = false },
+  { "tpope/vim-fugitive", event = "User AstroGitFile" },
+  { "tpope/vim-rhubarb", event = "User AstroGitFile" },
+  -- {
+  --   "prochri/telescope-all-recent.nvim",
+  --   dependencies = {
+  --     "nvim-telescope/telescope.nvim",
+  --     "kkharji/sqlite.lua",
+  --     -- optional, if using telescope for vim.ui.select
+  --     "stevearc/dressing.nvim",
+  --   },
+  --   opts = {
+  --     -- your config goes here
+  --   },
+  -- },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      file_types = { "markdown", "Avante" },
+    },
+    ft = { "markdown", "Avante" },
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      -- add any opts here
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    opts = function(_, opts)
+      opts.float_opts = {
+        width = vim.o.columns,
+        height = vim.o.lines - 4,
+      }
+    end,
+  },
+
+  -- == Examples of Overriding Plugins ==
+
+  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.enable_git_status = true
+      -- I think this is what fixes an issue with Fugitive (neo-tree auto opens
+      -- on opening Fugitive status)
+      opts.follow_current_file = false
+      opts.open_files_do_not_replace_types = { "terminal", "trouble", "qf", "fugitive" }
+      opts.hijack_netrw_behavior = "disabled"
+    end,
+  },
+  { "glepnir/oceanic-material" },
+  { "NTBBloodbath/doom-one.nvim" },
+  { "ayu-theme/ayu-vim" },
 }
